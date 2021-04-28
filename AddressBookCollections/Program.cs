8c3e065a6451dc7dafbb8ff76001ebd2a11b7461
vundelayamini <vundelayamini@gmail.com>
2021-04-28 11:34:14 +0530

    [yamini]Add: Welcome Message to AddressBook using collections

---
+++ AddressBookCollections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook Using Collections");
+        }
+    }
+}
